f7c23004ba9c8c86beabebd631d0d03d06aa4a78
Lucas S <lucasluke307@gmail.com>
2025-04-03 22:12:57 -0300

    Implementing integration tests for CustomerController

---
+++ src/test/java/com/food/utils/AuthData.java
@@ -0,0 +1,10 @@
+package com.food.utils;
+
+public class AuthData {
+    public static final Long ID = 1L;
+    public static final String NAME = "Lucas Scott";
+    public static final String EMAIL = "lucasluke307@gmail.com";
+    public static final String PASSWORD = "lucas123";
+    public static final String HASHED_PASSWORD = "$2a$10$Z0RSGZQpvYSSIUHv1p3LD.CZyGelDlqEyAbTy1KR5iDuyFwK2lx.G";
+    public static final String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsdWNhc2x1a2UzMDdAZ21haWwuY29tIiwiaWF0IjoxNzQzMjIwMzA3LCJleHAiOjE3NDMzMDY3MDd9.uPMgJ8scXRgUHEo70rnSpcn1AyG0D68yTtcnKpeDjLQ";
+}
