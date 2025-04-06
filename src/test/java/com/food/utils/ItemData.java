c6e0ed506e26800b55f62521e05f8470cdd03866
Lucas S <lucasluke307@gmail.com>
2025-04-06 14:34:24 -0300

    Merge branch 'dev'

--- src/test/java/com/food/utils/ItemData.java
+++
@@ -1,8 +0,0 @@
-package com.food.utils;
-
-public class ItemData {
-    public static final Long ID = 1L;
-    public static final String NAME = "Hamburguer";
-    public static final Double PRICE = 12.50;
-    public static final int QUANTITY = 1;
-}
